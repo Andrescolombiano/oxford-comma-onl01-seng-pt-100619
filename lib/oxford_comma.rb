f2502e25caa1d03f27093273f4e49a4efe8cd75c
Andres Carvajal <jc1993box@hotmail.com>
2019-10-31 04:25:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
-# def oxford_comma(array)
-#   if array.count == 1 
-#     array [0] 
-#   elsif array.count == 2 
-#     array.join
-#   else array.count > 2 
-#     last=array.pop
-    
-  
+def oxford_comma(array)
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  else array.length > 2
+    array[0..-2].join(", ") + ", and " + array[-1]
+  end
+end
